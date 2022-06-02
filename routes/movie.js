45dcf828784984d8f7e9765438a7e0dc3442a0a1
Velmurugan1425 <velmuruganb1401@gmail.com>
2022-06-02 18:16:57 +0530

    deploy herko

--- routes/movie.js
+++ routes/movie.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()  // it is a framwork help to create the router handler extend this routing to handle validation, handle 404 or other errors etc
 const movie = router
 const Movie = require('../module/movie')
-const conf = require('../config')  //import config modal to access the datas of config modal
 const jwt = require('jsonwebtoken');
 const movies = new Movie()
-
+const Table = require("../module/table")
+// const table = new Table()
 
 var Sessioncheck =async function (req, res, next) {
     var sessionObj = req.cookies['SESSION_ID']  //get data from sessionobj it present or not
@@ -31,8 +31,11 @@ var Sessioncheck =async function (req, res, next) {
 };
 
 
-movie.post('/movie/:action',Sessioncheck,function(req,res){ //sessioncheck goto the function check the session true execute the function
-    movies.perforam(req,res)
+movie.post('/movie/list',function(req,res){ //sessioncheck goto the function check the session true execute the function
+    Table.find({}, function (err, users) {
+        if (err) return res.status(404).json({ "status": false, 'result': err })
+        else res.json({ status: true, result: users })
+    });
 })
 
 module.exports = movie // export modue to access other files inside
